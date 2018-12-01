071919f40f5faa055989049cc41374800d3d827d
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-12-01 16:39:05 -0500

    Restructuring as a pure Python module (in progress)

---
+++ getkw/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for getkw."""
+
+__author__ = "Jonas Juselius, Roberto Di Remigio, Radovan Bast, Luca Frediani, Stig Rune Jensen, Ville Weijo"
+__copyright__ = "Copyright 2018, dev-cafe"
+__credits__ = [
+    "Jonas Juselius", "Roberto Di Remigio", "Radovan Bast", "Luca Frediani",
+    "Stig Rune Jensen", "Ville Weijo"
+]
+__license__ = "MPLv2"
+__version__ = "1.0.0"
+__maintainer__ = "Roberto Di Remigio, Radovan Bast, Stig Rune Jensen"
+__status__ = "Production"
+
+from .getkw import *  # isort:skip
